ec59a53081443d4ff7e31fedc3b820d2bbcfefd0
Danilo Ramirez <danilo0613@hotmail.com>
2023-02-08 08:54:00 -0500

    Spring heroku app config

---
+++ src/main/java/com/example/obSpringDeploy/ObSpringDeployApplication.java
@@ -0,0 +1,14 @@
+package com.example.obSpringDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringDeployApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ObSpringDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obSpringDeploy/ObSpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obSpringDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObSpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
